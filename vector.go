ff587f939af65a02038926596470002fb595d9e5
Konstantin Voykov <koykov0@gmail.com>
2025-11-06 23:22:50 +0300

    mark deprecated methods

--- vector.go
+++ vector.go
@@ -25,7 +25,7 @@ func (vec *Vector) Parse(s []byte) error {
 	return vec.parse(s, false)
 }
 
-func (vec *Vector) ParseStr(s string) error {
+func (vec *Vector) ParseString(s string) error {
 	return vec.parse(byteconv.S2B(s), false)
 }
 
@@ -33,7 +33,7 @@ func (vec *Vector) ParseCopy(s []byte) error {
 	return vec.parse(s, true)
 }
 
-func (vec *Vector) ParseCopyStr(s string) error {
+func (vec *Vector) ParseCopyString(s string) error {
 	return vec.parse(byteconv.S2B(s), true)
 }
 
